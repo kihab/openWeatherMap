944f51767ac9e418772a310d9bd4d77de816f814
Karim Ihab <karim.ihab@gmail.com>
2018-04-19 16:27:19 +0200

    - Implement Help Screen.

---
+++ OpenWeatherMap/Controllers/Help/HelpViewController.swift
@@ -0,0 +1,32 @@
+//
+//  HelpViewController.swift
+//  OpenWeatherMap
+//
+//  Created by Karim Ihab on 4/19/18.
+//  Copyright © 2018 Karim Ihab. All rights reserved.
+//
+
+import UIKit
+import WebKit
+
+class HelpViewContoller: UIViewController, WKNavigationDelegate {
+    
+    var webView: WKWebView!
+    
+    override func loadView() {
+        webView = WKWebView()
+        webView.navigationDelegate = self
+        webView.allowsBackForwardNavigationGestures = true
+        view = webView
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        let htmlPath = Bundle.main.path(forResource: "help", ofType: "html")
+        guard let path = htmlPath else { return }
+        let htmlUrl = URL(fileURLWithPath: path, isDirectory: false)
+        webView.loadFileURL(htmlUrl, allowingReadAccessTo: htmlUrl)
+        
+    }
+}
